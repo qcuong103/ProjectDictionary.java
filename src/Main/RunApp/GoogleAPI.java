3b42351b7f2ae2052e6cf457a5d96d3169ef9efa
quoccuong100300 <quoccuong100300@gmail.com>
2020-10-22 17:43:11 +0700

    hoan thien class GUIController

---
+++ src/Main/RunApp/GoogleAPI.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class GoogleAPI {
+
+//    public static void main(String[] args) throws IOException {
+//        String text = "Hello world!";
+//        //Translated text: Hallo Welt!
+//        System.out.println("Translated text: " + translate("en", "vi", text));
+//    }
+
+    public static String translate(String text) throws IOException {
+        String urlStr = "https://script.google.com/macros/s/AKfycbwBmVcf0EkYabebyujuwz9EsoyHuhrszUoHUBPae3m64f6Rogg/exec" +
+                "?q=" + URLEncoder.encode(text, "UTF-8") +
+                "&target=" + "vi" +
+                "&source=" + "en";
+        URL url = new URL(urlStr);
+        StringBuilder response = new StringBuilder();
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        return response.toString();
+    }
+}
